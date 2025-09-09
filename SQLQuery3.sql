69ad14c8d631045ac5fef092a69655074e6d1b72
Максим <122800954+maksli8@users.noreply.github.com>
2025-09-09 11:46:26 +0300

    Add files via upload

---
+++ SQLQuery3.sql
@@ -0,0 +1,135 @@
+PRINT 'INT'
+ --1--
+
+ DECLARE @A INT = 1;
+ PRINT @A
+
+  --2--
+
+ DECLARE @B INT = 2;
+ PRINT @B
+
+  --3--
+
+ DECLARE @C INT = 3;
+ PRINT @C
+
+  --4--
+
+ DECLARE @D INT = 4;
+ PRINT @D
+
+  --5--
+
+ DECLARE @E INT = 5;
+ PRINT @E
+
+ ---------------------------------------------
+ PRINT 'BIT'
+ --1--
+
+ DECLARE @bite1 INT = 1;
+ PRINT @bite1
+
+  --2--
+
+ DECLARE @bite2 INT = 2;
+ PRINT @bite2
+
+  --3--
+
+ DECLARE @bite3 INT = 3;
+ PRINT @bite3
+
+  --4--
+
+ DECLARE @bite4 INT = 4;
+ PRINT @bite4
+
+  --5--
+
+ DECLARE @bite5 INT = 5;
+ PRINT @bite5
+ ---------------------------------------------
+
+ PRINT 'money'
+ --1--
+
+ DECLARE @money1 INT = 1;
+ PRINT @money1
+
+  --2--
+
+ DECLARE @money2 INT = 2;
+ PRINT @money2
+
+  --3--
+
+ DECLARE @money3 INT = 3;
+ PRINT @money3
+
+  --4--
+
+ DECLARE @money4 INT = 4;
+ PRINT @money4
+
+  --5--
+
+ DECLARE @money5 INT = 5;
+ PRINT @money5
+
+ ---------------------------------------------
+
+ PRINT 'small'
+ --1--
+
+ DECLARE @small1 INT = 1;
+ PRINT @small1
+
+  --2--
+
+ DECLARE @small2 INT = 2;
+ PRINT @small2
+
+  --3--
+
+ DECLARE @small3 INT = 3;
+ PRINT @small3
+
+  --4--
+
+ DECLARE @small4 INT = 4;
+ PRINT @small4
+
+  --5--
+
+ DECLARE @small5 INT = 5;
+ PRINT @small5
+
+  ---------------------------------------------
+
+ PRINT 'decimal'
+ --1--
+
+ DECLARE @decimal1 INT = 1;
+ PRINT @decimal1
+
+  --2--
+
+ DECLARE @decimal2 INT = 2;
+ PRINT @decimal2
+
+  --3--
+
+ DECLARE @decimal3 INT = 3;
+ PRINT @decimal3
+
+  --4--
+
+ DECLARE @decimal4 INT = 4;
+ PRINT @decimal4
+
+  --5--
+
+ DECLARE @decimal5 INT = 5;
+ PRINT @decimal5
